1dbf5fb65b3d2cb554f62027d80736c799dc181c
Jinming Wu, Patrick <me@patrickwu.space>
2022-04-18 21:47:09 +0800

    adding full gallery support

--- app.py
+++ app.py
@@ -1,40 +1,53 @@
 from flask import Flask, redirect, render_template
 import requests, os, urllib
-from flask_bootstrap import Bootstrap5
+from flask_bootstrap import Bootstrap4
 
 app = Flask(__name__)
-bootstrap = Bootstrap5(app)
+bootstrap = Bootstrap4(app)
 
 
 BASE_URL = "http://localhost:41595"
 
-
-def get_thumbnail_path(id):
-    if id == "L20GYYPWRJM4O":
-        print("here")
+def get_media_paths(id, ext):
     ri = requests.get(BASE_URL + "/api/item/thumbnail?id=" + id, {redirect: 'follow'})
     full_path = urllib.parse.unquote(ri.json()['data'])
+    head, tail = os.path.split(full_path)
+    last_files = []
+    for a in os.listdir(head):
+        a = str(a)
+        if a == tail or a == "metadata.json":
+            continue
+        last_files.append(a)
+    if len(last_files) == 0:
+        last_files.append(tail)
+    raw_full_path = os.path.join(head, last_files[-1])
     os.makedirs('static/thumbnails/', exist_ok=True)
-    thumbnail_path = 'static/thumbnails/' + id + "_thumbnail.png"
+    os.makedirs('static/full/', exist_ok=True)
+    thumbnail_path = "static/thumbnails/{}.png".format(id)
+    raw_path = "static/full/{}.{}".format(id, ext)
     if os.path.exists(thumbnail_path):
-        os.remove(thumbnail_path)
+        os.unlink(thumbnail_path)
     os.symlink(full_path, thumbnail_path)
-    return "/" + thumbnail_path
+    if os.path.exists(raw_path):
+        os.unlink(raw_path)
+    os.symlink(raw_full_path, raw_path)
+    return "/" + thumbnail_path, "/" + raw_path
 
 @app.route('/folders/<item>')
 def list_items(item):
+    data = requests.get(BASE_URL + "/api/library/info", {redirect: 'follow'}).json()
+    name = [x['name'] for x in data['data']['folders'] if x['id'] == item][-1]
     r = requests.get(BASE_URL + "/api/item/list?limit=10000&folders=" + item, {redirect: 'follow'})
     data = r.json()
     list = []
+    c = 0
     for x in data['data']:
         if x['ext'] in ('jpg', 'png', 'gif'):
-            list.append({'name': x['name'], 'thumbnail': get_thumbnail_path(x['id'])})
+            thumbnail, full_image = get_media_paths(x['id'], x['ext'])
+            list.append({'name': x['name'], 'thumbnail': thumbnail, 'image': full_image, 'count': c})
+            c+= 1
     
-    return render_template('list_items.html', list=list)
-
-@app.route('/clean')
-def clear_cache():
-    os.rmdir('static/thumbnails/')
+    return render_template('list_items.html', title=name, list=list)
 
 @app.route('/', methods=['GET'])
 def show_main():
